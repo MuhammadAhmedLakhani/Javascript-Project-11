c22cf675e624a8d01d4239cfa991d1c9a44a6ad0
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-17 22:09:56 +0500

    AttendenceApp:Validation added

--- app.js
+++ app.js
@@ -1,120 +1,48 @@
 
-let fileInput = document.getElementById("csvFileInput")
-let thead = document.getElementById("thead")
-let tbody = document.getElementById("tbody")
+let nameValidation = false;
+let emailValidation = false;
+
+function register() {
+
+    let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    let input = document.getElementById("name")
+    let name = input.value.trim()
+    let input1 = document.getElementById("email")
+    let email = input1.value.trim()
 
 
-fileInput.addEventListener("change", readFile)
+    if (name) {
+        nameValidation = true
+        input.value = ""
+    } else {
+        alert("PLease enter Name correctly")
+    }
 
-function readFile() {
-    console.log("function running")
+    if (email) {
+        let status = emailRegex.test(email)
+        if (status) {
+            emailValidation = true
+            input1.value = ""
+        } else {
+            alert("PLease enter Email correctly")
 
-    var file = fileInput.files[0]
+        }
 
-    console.log(file)
+    } else {
+        alert("PLease enter Email correctly")
 
-    var reader = new FileReader()
+    }
 
-    reader.readAsText(file)
+    if(nameValidation === true && emailValidation === true) {
 
-    reader.addEventListener("load", function () {
-        let data = reader.result
-        let structureData = dataFormation(data)        
-        console.log(structureData)
-        tableUIFormation(structureData)
+        window.location.assign("index1.html")
 
-    })
-
-}
-
-function dataFormation(data) {
-
-
-
-    let resultArray = data.split("\n")
-    
-    let cleanLines = resultArray.map((line) => line.trim())
+    }
 
 
-    let headings = cleanLines[0].split(",")
 
 
-    let dataLines = cleanLines.slice(1)
-
-    let results = dataLines.map((line, i) => {
-        const values = line.split(",")
-
-
-        let obj = {}
-
-        headings.map((heading, index) => {
-            obj[heading] = values[index]
-        })
-        return obj
-
-    })
-    return results
-
-    
-
-}
-
-
-function tableUIFormation(data) {
-
-
-    let tableRow = document.createElement("tr")
-
-    let headers = Object.keys(data[0])
-
-    console.log(headers)
-    tableRow.innerHTML= headers.map((head) =>{
-        return `<th>${head}</th>`
-    }).join("")  
-
-    thead.appendChild(tableRow)
-
-    data.forEach((student,ind) => {
-        let row = document.createElement("tr")
-        console.log(student)   
-        let cellsData = Object.values(student).map((value)=>{
-            return `<td>${value}</td>`
-        }).join("")
-
-        row.innerHTML =  cellsData
-
-        tbody.appendChild(row)
-
-
-    });
-
-
-    console.log(tbody)
-
-    var header =  document.getElementById("input")
-    header.setAttribute("class","hide")
-
-    fileInput.value = ""
-    var backbtn = document.getElementById("back")
-    backbtn.setAttribute("class","back")
-    var heading = document.getElementById("heading")
-    console.log(heading)
-    heading.setAttribute("class","heading")
-
-}
-
-
-function back() {
-
-    var attendanceTable = document.getElementById("attendanceTable")
-    attendanceTable.setAttribute("class","hide")
-    var header =  document.getElementById("input")
-    header.setAttribute("class","input")
-     var backbtn = document.getElementById("back")
-    backbtn.setAttribute("class","hide")
-    
-    
-}
 
 
 
+}
